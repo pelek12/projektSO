bbb1b02f86cf9af84822d0c00d606016413d9d3c
Kacper Cierzniewski <kac1234@gmail.com>
2017-06-11 12:09:20 +0200

    still fighting with basics

--- projekt.cpp
+++ projekt.cpp
@@ -7,31 +7,45 @@ char m[10];
 char n[10];
 int zrodlo1=0;
 int zrodlo2=0;
+char macierz[20][20];
 void producent(){
 	
 }
 void przetwarzacz(){
-	zrodlo1=std::rand()%atoi(m);
-	zrodlo2=std::rand()%atoi(m);
+
 
 }
 void ncurses(){
-	
-}
+for(int i=0; i < 20; i++){
+
+    for(int j=0; j < 20; j++){
+        printw("%c",macierz[i][j]);
+    }
+    printw("\n");
+}}
 int main(){
 		srand( time( NULL ) );
+for(int i=0; i < 20; i++){
 
+    for(int j=0; j < 20; j++){
+        macierz[i][j]=' ';    
+    }
+}
 	initscr();
-		printw("podaj szerokosc macierzy");
+	printw("podaj szerokosc macierzy");
 	getstr(m);
 	printw("podaj wysokosc macierzy");
 	getstr(n);
+	zrodlo1=std::rand()%atoi(m);
+	zrodlo2=std::rand()%atoi(m);
   auto przetwarzaczThread = std::thread(przetwarzacz); 
   auto producentThread = std::thread(producent); 
-  auto ncursesThread = std::thread(ncurses); 
+		clear();
+        
+	macierz[10][10]='d';
+    refresh();
+       ncurses();
 
-	clear();
-	mvprintw(zrodlo1,zrodlo2,"%d %d",zrodlo1,zrodlo2);
 	getch();
 	endwin();
 }
