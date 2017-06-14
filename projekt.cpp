e5288a2b7e3c5eb76868b0060092f616fbccfc34
Kacper Cierzniewski <kac1234@gmail.com>
2017-06-14 13:44:56 +0200

    moving elements in matrix is working

--- projekt.cpp
+++ projekt.cpp
@@ -3,81 +3,96 @@
 #include <ctime>
 #include <thread>
 #include <chrono>
+#include <queue>
+#include <mutex>
+#include <time.h>
 char m[10];
 char n[10];
 char temp=' ';
 char temp2=' ';
 int licznik=0;
 int zrodlo1=0;
-int zrodlo2=0;
-char macierz[3][3];
+int zrodlo2=0;	
+char macierz[10][10];
+	std::mutex mx;
+std::queue <int> kolejka1;
+std::queue <int> kolejka2;
 void producent(){
-	
+	while (true){
+std::this_thread::sleep_for(std::chrono::milliseconds(500));
+ mx.lock();
+	kolejka1.push(std::rand()%10);
+	kolejka2.push(std::rand()%10);
+ mx.unlock();
+
+	}
+		
 }
 void ncurses(){
-for(int i=0; i < 3; i++){
-
-    for(int j=0; j < 3; j++){
-		if (macierz[i][j]==' ')
-		printw("%c",' ');
-			else
-        printw("%d",macierz[i][j]);
-    }
-    printw("\n");
-}}
-void przetwarzacz(){
-	  std::this_thread::sleep_for(std::chrono::seconds(4)); // sleep(4);
-
- for(int i=0; i < 3; i++){
-	 temp2=temp;
-	 temp=macierz[i][2];
-	 for(int j=0; j <1 ; j++){
-	 	macierz[i][j+2]=macierz [i][j+1];
-        macierz[i][j+1]=macierz[i][j];
-		macierz[i][j]= temp2;
-    }	
-        
+	while(true){
+		  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		  clear();
+				for(int i=0; i < 10; i++){
+	 for(int j=9; j >0 ; j--){
+              macierz[i][j] = macierz[i][j-1];
+	 }			macierz[i][0]= ' ';
+				}
 
     
-}
-        macierz[0][0]=' ';
+
+for(int i=0; i < 10; i++){
+
+    for(int j=0; j < 10; j++){
+		if (macierz[i][j]==' ')
+		printw(" %c",' ');
+			else
+        printw(" %c",macierz[i][j]);
+    }
+    printw("\n");
 
 }
+			refresh();
+	}}				  
+void przetwarzacz(){
+	while (true){
+	  std::this_thread::sleep_for(std::chrono::milliseconds(500)); // sleep(4);
+ 		 mx.lock();
+	     macierz[kolejka1.front()][kolejka2.front()]='+';
+ 
+		kolejka1.pop();
+		kolejka2.pop();
+		 mx.unlock();
+	
+	}	
+
+
+	}
+
 
 int main(){
 		srand( time( NULL ) );
-for(int i=0; i < 3; i++){
+for(int i=0; i < 10; i++){
 
-    for(int j=0; j < 3; j++){
-      macierz[i][j]=licznik;
-	licznik++;
-    }
-}
-	initscr();
-	printw("podaj szerokosc macierzy");
-	getstr(m);
-	printw("podaj wysokosc macierzy");
-	getstr(n);
-	zrodlo1=std::rand()%atoi(m);
-	zrodlo2=std::rand()%atoi(m);
- // auto przetwarzaczThread = std::thread(przetwarzacz); 
-//  auto producentThread = std::thread(producent); 
-		clear();
-        
-      // auto ncursesThread= std::thread(ncurses);
-    ncurses();
-	refresh();
-    
-    getch();
-    		clear();
-while (true){
-        przetwarzacz();
-		refresh();
-	ncurses();
+    for(int j=0; j < 10; j++){
+      macierz[i][j]=' ';
 	}
-	refresh();
 
-    	getch();
+}
+	
+	initscr();
+//	printw("podaj szerokosc macierzy");
+	//getstr(m);
+	//printw("podaj wysokosc macierzy");
+	//getstr(n);
+	//zrodlo1=std::rand()%atoi(m);
+	//zrodlo2=std::rand()%atoi(m);
+  	auto przetwarzaczThread = std::thread(przetwarzacz); 
+ 	auto producentThread = std::thread(producent); 
+    auto ncursesThread = std::thread(ncurses); 
+        przetwarzaczThread.join();
+      	ncursesThread.join();
+		producentThread.join();
+getch();
 
 	endwin();
 }
